dda6dcde3da046d184507a250e6fbb441b55b51e
Jordunw <forestwmck@gmail.com>
2023-12-10 20:00:12 -0700

    basic game

---
+++ player/scripts/PlayerCamera.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+var spd = 15
+
+func _physics_process(_delta: float) -> void:
+	if(Input.is_action_pressed("ui_right")):
+		global_position.x += spd
+	if(Input.is_action_pressed("ui_left")):
+		global_position.x -= spd
+	if(Input.is_action_pressed("ui_up")):
+		global_position.y -= spd
+	if(Input.is_action_pressed("ui_down")):
+		global_position.y += spd
