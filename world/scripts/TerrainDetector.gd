d6b231a29645c0be0f11e9ae4ae543c642929e40
Jordunw <forestwmck@gmail.com>
2023-12-12 23:55:01 -0700

    portal wip

---
+++ world/scripts/TerrainDetector.gd
@@ -0,0 +1,22 @@
+class_name TerrainDetector
+extends Area2D
+
+signal entered_portal;
+
+enum TerrainType {
+	
+}
+
+const terrain_bitmask = 255;
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
